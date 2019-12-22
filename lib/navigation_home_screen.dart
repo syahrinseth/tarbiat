ff0baefc357a7c470ad1bc6c75d37e93863d3607
Syahrin Seth <syahrin_seth@yahoo.com>
2019-12-22 20:17:28 +0800

    Update: Habit home page.

---
+++ lib/navigation_home_screen.dart
@@ -0,0 +1,76 @@
+import 'package:tabiat/app_theme.dart';
+import 'package:tabiat/custom_drawer/drawer_user_controller.dart';
+import 'package:tabiat/custom_drawer/home_drawer.dart';
+import 'package:tabiat/feedback_screen.dart';
+import 'package:tabiat/help_screen.dart';
+import 'package:tabiat/home_screen.dart';
+import 'package:tabiat/invite_friend_screen.dart';
+import 'package:flutter/material.dart';
+
+class NavigationHomeScreen extends StatefulWidget {
+  @override
+  _NavigationHomeScreenState createState() => _NavigationHomeScreenState();
+}
+
+class _NavigationHomeScreenState extends State<NavigationHomeScreen> {
+  Widget screenView;
+  DrawerIndex drawerIndex;
+  AnimationController sliderAnimationController;
+
+  @override
+  void initState() {
+    drawerIndex = DrawerIndex.HOME;
+    screenView = const MyHomePage();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: AppTheme.nearlyWhite,
+      child: SafeArea(
+        top: false,
+        bottom: false,
+        child: Scaffold(
+          backgroundColor: AppTheme.nearlyWhite,
+          body: DrawerUserController(
+            screenIndex: drawerIndex,
+            drawerWidth: MediaQuery.of(context).size.width * 0.75,
+            animationController: (AnimationController animationController) {
+              sliderAnimationController = animationController;
+            },
+            onDrawerCall: (DrawerIndex drawerIndexdata) {
+              changeIndex(drawerIndexdata);
+            },
+            screenView: screenView,
+          ),
+        ),
+      ),
+    );
+  }
+
+  void changeIndex(DrawerIndex drawerIndexdata) {
+    if (drawerIndex != drawerIndexdata) {
+      drawerIndex = drawerIndexdata;
+      if (drawerIndex == DrawerIndex.HOME) {
+        setState(() {
+          screenView = const MyHomePage();
+        });
+      } else if (drawerIndex == DrawerIndex.Help) {
+        setState(() {
+          screenView = HelpScreen();
+        });
+      } else if (drawerIndex == DrawerIndex.FeedBack) {
+        setState(() {
+          screenView = FeedbackScreen();
+        });
+      } else if (drawerIndex == DrawerIndex.Invite) {
+        setState(() {
+          screenView = InviteFriend();
+        });
+      } else {
+        //do in your way......
+      }
+    }
+  }
+}
